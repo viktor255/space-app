392a2f9287abd628dbcd7ce63cac6c4d57c55034
viktor255 <viktor.lehotsky@gmail.com>
2018-12-04 04:07:02 +0100

    overovanie emailu token

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
+const Token = require('../models/token');
+const nodemailer = require('nodemailer');
+const crypto = require('crypto');
 
 const router = express.Router();
 
@@ -15,8 +18,45 @@ router.post('/signup', (req, res, next) => {
       user.save()
         .then(result => {
           console.log('User created');
-          console.log(result);
-          res.status(201).send();
+          // console.log(result);
+
+          // Create a verification token for this user
+          var token = new Token({_userId: user._id, token: crypto.randomBytes(16).toString('hex')});
+
+          // Save the verification token
+          token.save(function (err) {
+            if (err) {
+              console.log('error token');
+              return res.status(500).json({error: err.message});
+            }
+
+            // Send the email
+            var transporter = nodemailer.createTransport({
+              host: 'smtp.sendgrid.net',
+              port: 587,
+              secure: false,
+              auth: {
+                user: 'apikey',
+                pass: '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'
+              }
+            });
+
+            var mailOptions = {
+              from: 'no-reply@space-app.cz',
+              to: user.email,
+              subject: 'Space-app: Account Verification Token',
+              text: 'Hello,\n\n' + 'Please verify your account by clicking the link: \nhttp:\/\/'
+                + req.headers.host + '\/confirmation\/' + token.token + '.\n'
+            };
+            transporter.sendMail(mailOptions, function (err) {
+              if (err) {
+                console.log('error message');
+                console.log(err);
+                return res.status(500).json({error: err.message});
+              }
+              res.status(201).send();
+            });
+          });
         })
         .catch(err => {
           res.status(500).json({
@@ -31,14 +71,17 @@ router.post('/login', (req, res, next) => {
   User.findOne({email: req.body.email})
     .then(user => {
       if (!user) {
-        return res.status(401).json({message: 'Auth1 failed'});
+        return res.status(401).json({message: 'Invalid email of password1'});
       }
       fetchedUser = user;
       return bcrypt.compare(req.body.password, user.password);
     })
     .then(result => {
       if (!result) {
-        return res.status(401).json({message: 'Auth2 failed'});
+        return res.status(401).json({message: 'Invalid email of password2'});
+      }
+      if (!fetchedUser.isVerified) {
+        return res.status(401).json({message: 'User is not verified'});
       }
       const token = jwt.sign({
         email: fetchedUser.email,
@@ -50,7 +93,7 @@ router.post('/login', (req, res, next) => {
           email: fetchedUser.email,
           token: token,
           role: fetchedUser.role,
-          expiresIn: Date.now() + 3600*1000
+          expiresIn: Date.now() + 3600 * 1000
         }
       });
 
@@ -58,6 +101,42 @@ router.post('/login', (req, res, next) => {
     .catch(err => {
       return res.status(401).json({err: err});
     });
+});
+
+router.get('/confirmation/:token', (req, res, next) => {
+
+  // Check for validation errors
+  // var errors = req.validationErrors();
+  // if (errors) return res.status(400).send(errors);
+
+  // Find a matching token
+  Token.findOne({token: req.params.token}, function (err, token) {
+    if (!token) return res.status(400).send({
+      type: 'not-verified',
+      msg: 'We were unable to find a valid token. Your token my have expired.'
+    });
+
+    // If we found a token, find a matching user
+    User.findOne({_id: token._userId}, function (err, user) {
+      if (!user) return res.status(400).send({msg: 'We were unable to find a user for this token.'});
+      if (user.isVerified) return res.status(400).send({
+        type: 'already-verified',
+        msg: 'This user has already been verified.'
+      });
+
+      // Verify and save the user
+      user.isVerified = true;
+      user.save(function (err) {
+        if (err) {
+          return res.status(500).send({msg: err.message});
+        }
+        res.status(200).send("The account has been verified. Please log in.");
+      });
+    });
+  });
+
+});
+router.post('/resend', (req, res, next) => {
 });
 
 module.exports = router;
